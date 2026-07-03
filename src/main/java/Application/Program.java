be979fb75f46fdbe5caf95c62576f3779efe866a
EllianLIma <elliancesar23@gmail.com>
2026-07-03 19:39:38 -0300

    findById implementation

--- src/main/java/Application/Program.java
+++ src/main/java/Application/Program.java
@@ -3,9 +3,13 @@ package Application;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
    public static void main(String[] args) {
+       SellerDao sellerDao = DaoFactory.createSellerDao();
+       Seller seller = sellerDao.findById(3);
 
+       System.out.println(seller);
     }
 }
