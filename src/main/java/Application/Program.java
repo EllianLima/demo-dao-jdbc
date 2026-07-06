8270b9e31482af5c6f9f166ea41f1b7db2d081e0
EllianLIma <elliancesar23@gmail.com>
2026-07-06 09:44:24 -0300

    update implementation

--- src/main/java/Application/Program.java
+++ src/main/java/Application/Program.java
@@ -29,9 +29,16 @@ public class Program {
            System.out.println(obj);
        }
 
-       System.out.println("====== test 4: findAll ========");
+       System.out.println("====== test 4: seller insert ========");
        Seller newSeller = new Seller(null, "Jhon", "greg@gmail.com", LocalDate.now(), 4000.00, department );
        sellerDao.insert(newSeller);
        System.out.println("Inserted, new Id = " + newSeller.getId());
+
+       System.out.println("====== test 5: seller update ========");
+       seller = sellerDao.findById(1);
+       seller.setName("Matha Waine");
+       sellerDao.update(seller);
+       System.out.println("Update completed");
+
     }
 }
